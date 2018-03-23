8d7a06b8f08b67e4a54adcf57018d7df68a2fbcf
ivanblekk <ivan04blek@live.nl>
2018-03-23 11:54:47 +0100

    ser
    fdfdsg

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -20,45 +20,19 @@ namespace Prototype.NetworkLobby
 
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
             if (LobbyTopPanel.IsInGame()) 
                 {
                 
 
-                camPosition =  new Vector2(GameObject.Find("Main Camera").transform.position.x, GameObject.Find("Main Camera").transform.position.y);
-                mainCamera = GameObject.Find("Main Camera");
+                
 
                 if (player != null)
                 {
                     transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, -10);
 
-                    /*if (!LobbyTopPanel.IsDisplayed())
-                    {
-                        Cursor.lockState = CursorLockMode.Locked;
-                        Vector2 prevMousePos = mousePos;
-                        mousePos = new Vector2(GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition).x, GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition).y);
-                        mouseAngle = Vector2.Angle(prevMousePos, mousePos);
-                        float mouseMovementHorizontal = Input.GetAxis("Mouse X");
-                        float mouseMovementVertical = Input.GetAxis("Mouse Y");
-                        Vector2 movement = new Vector2(mouseMovementHorizontal, mouseMovementVertical);
-                        Vector2 camPosition = new Vector2(GameObject.Find("Main Camera").transform.position.x, GameObject.Find("Main Camera").transform.position.y);
-                        playerPosition = new Vector2(player.transform.position.x, player.transform.position.y);
-                        offset = camPosition - playerPosition;
-                        magnitude = offset.magnitude;
-                        if (magnitude <= MaxdistanceBetweenPlayerAndCam)
-                        {
-                            camPosition += movement * 0.3f;
-                            mainCamera.transform.position = new Vector3(camPosition.x, camPosition.y, mainCamera.transform.position.z);
-                        }
-                        else
-                        {
-                            Vector2 mouseToPlayer = new Vector2(player.transform.position.x - mainCamera.transform.position.x, player.transform.position.y - mainCamera.transform.position.y);
-                            mouseToPlayer.Normalize();
-                            camPosition += mouseToPlayer * 1 / (MaxdistanceBetweenPlayerAndCam * (MaxdistanceBetweenPlayerAndCam - mouseToPlayer.magnitude));
-                            mainCamera.transform.position = new Vector3(camPosition.x, camPosition.y, mainCamera.transform.position.z);
-                        }
-                    }*/
+                  
                 }
                 else
                 {
